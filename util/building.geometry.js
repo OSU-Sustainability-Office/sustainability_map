bbf725ac8f4d4574418100dcd65fefd66d3d2742
MilanDonhowe <milanaugust@gmail.com>
2021-07-07 14:40:25 -0700

    Fixed building query; more buildings show now

--- util/building.geometry.js
+++ util/building.geometry.js
@@ -9,7 +9,7 @@ const fs = require('fs')
 const path = require('path')
 
 // Query selects all OSU university buildings @ corvallis
-const overpass_query = '[out:json][bbox:44.5501,-123.3111,44.5756,-123.2556];(way[building="university"];node(w););out body;'
+const overpass_query = '[out:json];(way["name"]["leisure"!~".*"]["landuse"!~".*"](area:2922709422);node(w););out;'
 const overpass_api = 'https://lz4.overpass-api.de/api/interpreter'
 
 // Downloads JSON OSM features and converts them to GeoJSON
@@ -33,7 +33,7 @@ console.log('Downloading data from Open Street Maps...')
 DownloadBuildingFeatures(overpass_query, overpass_api)
   .then(data => {
     // Remove excess points
-    data.features = data.features.filter(feat => feat.geometry.type !== 'Point')
+    data.features = data.features.filter(feat => feat.geometry.type === 'Polygon')
 
     const parent_dir = __dirname.split(path.sep).slice(0, -1).join(path.sep)
     const filename = path.join(parent_dir, 'src', 'assets', 'GeoJSON', 'building_features.json')
