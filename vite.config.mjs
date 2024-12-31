5141060471370823246c85af69d1229f9997fbbb
s-egge <ssteele836@gmail.com>
2024-12-31 11:52:34 -0800

    Pathing fix

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,7 +10,7 @@ export default ({ mode }) => {
 
   return defineConfig({
     plugins: [vue(), svgLoader(), hjsonPlugin()],
-    base: '/sustainability_map/',
+    base: process.env.VITE_HOST_ADDRESS,
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src')
