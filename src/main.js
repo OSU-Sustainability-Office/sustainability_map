f66c79197de3e841792cc6651561ed592c8e8f58
Brandon Lee <brandovlee@gmail.com>
2025-10-19 17:58:20 -0700

    remove vue-i18n

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import { createI18n } from 'vue-i18n'
 
 // The order in which we load these leaflet css files matters
 // https://github.com/ghybs/leaflet-defaulticon-compatibility
@@ -13,16 +12,10 @@ import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css' // Re-uses images from ~leaflet package
 import '@/assets/leaflet-global-override.scss'
 
-const i18n = createI18n({
-  locale: 'en',
-  messages: {}
-})
-
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
-app.use(i18n)
 app.use(ElementPlus)
 
 // register all Element Plus icons globally
